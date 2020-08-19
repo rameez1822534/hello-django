14f78db025ce51711955545b369297cee3c5ab8c
rameez1822534 <ch_rameez89@hotmail.com>
2020-08-19 00:23:18 +0000

    item rename and edit

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render 
-
+from .models import item
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items =item.objects.all()
+    context = {
+        'items' : items
+    }
+    return render(request, 'todo/todo_list.html', context)
