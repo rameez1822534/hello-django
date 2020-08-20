a9eb757cea355492c8bedea4f33e6d9202d7d7ad
rameez1822534 <ch_rameez89@hotmail.com>
2020-08-20 11:58:25 +0000

    edit html added

--- todo/views.py
+++ todo/views.py
@@ -1,20 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import item
+from .forms import itemForm
 # Create your views here.
 
 
 def get_todo_list(request):
-    items =item.objects.all()
+    items = item.objects.all()
     context = {
-        'items' : items
+        'items': items
     }
     return render(request, 'todo/todo_list.html', context)
 
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        item.objects.create(name=name, done=done)
-        return redirect('get.todo_list')
-    return render(request, 'todo/add_item.html')
+        form = itemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = itemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    Item = get_object_or_404(item, id= item_id)
+    if request.method == 'POST':
+        form = itemForm(request.POST, instance= Item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = itemForm(instance =Item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
